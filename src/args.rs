bf1e0e3ca7041c378d7d889105082c3219a957bb
Rob Saunders <rob@wearebrandnew.com>
2019-01-27 16:47:41 +0800

    turned off args for now in order to develop positions feature

--- src/args.rs
+++ src/args.rs
@@ -1,13 +1,15 @@
-use clap::{App, ArgMatches, AppSettings};
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+// use cryptotrader::config::APIConfig;
+use clap;
+
 use crate::error::*;
 
-pub fn parse() -> CliResult<&'static str> {
+pub fn parse() -> CliResult<String> {
     let yaml = load_yaml!("../cli.yml");
-    let matches = App::from_yaml(yaml)
-        .setting(AppSettings::SubcommandRequiredElseHelp)
+    let matches = clap::App::from_yaml(yaml)
+        // .setting(AppSettings::SubcommandRequiredElseHelp)
         .get_matches();
 
     if matches.is_present("verbose") {
@@ -16,14 +18,18 @@ pub fn parse() -> CliResult<&'static str> {
     }
 
     // args have successfully parsed so we can start loading config etc.
-    let conf = cryptotrader::config::read(false)?;
+    let conf = cryptotrader::config::read()?;
+    let keys = &conf.exchange["binance"];
+    let client = cryptotrader::exchanges::binance::connect(&keys.api_key, &keys.secret_key);
 
-    match matches.subcommand() {
-        ("positions", Some(m)) => parse_positions(m),
-        _ => { Err(Box::new(CliError::InvalidCommand)) },
-    }
+    super::commands::positions(client)
+
+    // match matches.subcommand() {
+    //     ("positions", Some(m)) => parse_positions(m, conf.exchange["binance"].clone()),
+    //     _ => { Err(Box::new(CliError::InvalidCommand)) },
+    // }
 }
 
-fn parse_positions(_matches: &ArgMatches) -> CliResult<&'static str> {
-    super::commands::positions(["hi"].to_vec())
-}
+// fn parse_positions(_matches: &ArgMatches, _conf: APIConfig) -> CliResult<String> {
+//     super::commands::positions()
+// }
--- src/commands.rs
+++ src/commands.rs
@@ -1,5 +1,39 @@
 use crate::error::*;
+use crate::utils::*;
 
-pub fn positions(_pairs: Vec<&str>) -> CliResult<&'static str> {
-    Ok("positions")
+use cryptotrader;
+use cryptotrader::exchanges::ExchangeAPI;
+use cryptotrader::models::*;
+use cryptotrader::presenters::*;
+
+// enum PositionStyle {
+// 	Normal,
+// 	Verbose,
+// 	Ticker,
+// }
+
+pub fn positions<E>(client: E) -> CliResult<String> where E:ExchangeAPI {
+	let pairs = vec!["NULSBTC".to_string(), "WAVESBTC".to_string()];
+
+	let prices = client.prices()?;
+	let btc_price_in_usd = *(prices.get("BTCUSDT").unwrap_or(&0.0));
+	// let result_buffer = Vec::new();
+
+	for pair in pairs.clone() {
+		if let Ok(orders) = client.trades_for(&pair) {
+			let current_price = *(prices.get(&pair).unwrap_or(&0.0));
+			let grouped_orders = cryptotrader::models::group_by_price(orders.clone());
+			let positions = Position::new(grouped_orders);
+
+			if let Some(position) = positions.last() {
+				let pp = PositionPresenter{ position: position.clone(), current_price, btc_price_in_usd };
+				println!("PRICE: {:?}", pp);
+			}
+		}
+	}
+	
+
+
+    Ok(iter_map(pairs, {|pair| format!("[{}]", pair)})
+    	.join(" "))
 }
