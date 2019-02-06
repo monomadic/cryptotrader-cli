556729683b2fe0be90a2c2e63c6dd070818df02f
Rob Saunders <rob@wearebrandnew.com>
2019-02-06 15:01:05 +0800

    slightly improved display of prices

--- src/commands/pairs.rs
+++ src/commands/pairs.rs
@@ -6,5 +6,9 @@ use cryptotrader::{ exchanges::* };
 pub fn pairs<E>(client: E) -> CliResult<String> where E:ExchangeAPI {
     let prices = client.all_pairs()?;
 
-    Ok(format!("{:?}", prices))
+    let result = prices.into_iter().map(|pair| {
+        format!("{}-{} {}", pair.symbol, pair.base, pair.price)
+    }).collect::<Vec<String>>().join(" :: ");
+
+    Ok(result)
 }
